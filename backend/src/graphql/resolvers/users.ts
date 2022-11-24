84ba65baadc9fda05b5a864476f73f5370f8dc9f
Heder Alves <alvesheder92@gmail.com>
2022-11-24 15:10:31 -0300

    feat
    backend, mutation delete

--- backend/src/graphql/resolvers/users.ts
+++ backend/src/graphql/resolvers/users.ts
@@ -1,10 +1,12 @@
-import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Mutation, Query, Resolver } from "type-graphql";
 import { user } from "../../model/User";
 import { v4 as uuid } from 'uuid'
+import { Context } from "vm";
 
 @Resolver()
 export class UserResolver {
     private data: user[] = [];
+    service: any;
 
     @Query(() => [user])
     async user() {
@@ -18,5 +20,13 @@ export class UserResolver {
         const user = { id: uuid(), name }
         this.data.push(user)
         return user;
+    }
+
+    @Mutation(() => user)
+    async removeUser(
+        @Ctx() context: Context,
+        @Arg('userId') userId: string
+    ): Promise<user> {
+        return this.service.remove(context, userId)
     }
 }
