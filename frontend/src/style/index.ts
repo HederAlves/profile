4c8574b0072f93a11f4d5576d76e296018959c07
Heder Alves <alvesheder92@gmail.com>
2022-12-01 23:39:54 -0300

    chore

--- frontend/src/style/index.ts
+++ frontend/src/style/index.ts
@@ -11,17 +11,22 @@ export const Card = styled.section`
     display: flex;
     flex-direction: column;
     text-align: center;
-    padding: 50px;
+    padding: 30px 50px 60px 50px;
     border: 0.1px solid gray;
     border-radius: 10px;
     box-shadow: 5px 10px gray;
-    width: 100px;
-    height: 50px;
+    width: 150px;
+    height: 100px;
     margin: 5px;
 `
-export const Button = styled.button `
+export const Button = styled.button`
     background-color: blue;
     padding: 5px;
     margin: 10px 10px;
     color: white;
+`
+export const Input = styled.input`
+    width: 80px;
+    margin-top: 5px;
+    height:10px
 `
