a1fca9230776c95520e0c69742d095565ce881d0
Heder Alves <alvesheder92@gmail.com>
2022-12-02 00:53:08 -0300

    chore
    frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,30 @@
 import { useQuery } from "@apollo/client";
 import type { User } from '../src/types/index';
 import { GET_USERS } from '../src/graphql/queries/Users'
-import { NewUserForm } from "./components/NewUserForm";
-import { DeleteUser } from "./components/DeleteUser";
 import { Card, Layout } from "./style";
+import  NewUserForm  from "./components/NewUserForm";
+import DeleteUser from "./components/DeleteUser";
+import UpdateUser from "./components/UpdateUser";
 
 function App() {
   const { data, loading } = useQuery<{ user: User[] }>(GET_USERS);
 
-
   if (loading) {
     return <p>Carregando...</p>;
-  } 
-    return (
-      <>
-        <NewUserForm />
-        <Layout>
-            { data?.user.map( _user => <Card key={ _user.id}>{ _user.name}<DeleteUser /></Card>) }
-        </Layout>
-      </>
-    )
+  }
+
+  return (
+    <>
+      <NewUserForm />
+      <Layout>
+        {data?.user.map(_user =>
+          <Card key={_user.id}>{_user.name}
+            <DeleteUser id={_user.id} />
+            <UpdateUser name={_user.name} id={_user.id} />
+          </Card>)}
+      </Layout>
+    </>
+  )
 }
 
 export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-
 import { ApolloProvider } from '@apollo/client'
 import { client } from './lib/apollo'
 
