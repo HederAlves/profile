65aa1e8a8713589115cdeadac4f1786f71cc031e
Heder Alves <alvesheder92@gmail.com>
2022-12-03 21:10:21 -0300

    feat

--- frontend/src/components/users/ViewUsers.tsx
+++ frontend/src/components/users/ViewUsers.tsx
@@ -6,24 +6,23 @@ import Create from "./Create";
 import Delete from "./Delete";
 import Update from "./Update";
 
-
-
 const View = () => {
-    
-    const { data, loading } = useQuery<{ user: User[] }>(GET_USERS);
+  const { data, loading } = useQuery<{ user: User[] }>(GET_USERS);
 
-    if (loading) {
+  if (loading) {
       return <p>Carregando...</p>;
-    }
-
+  }
   return (
     <>
     <Create id={""} name={""} />
     <Layout>
         {data?.user.map(_user => 
-        <Card key={_user.id}>{_user.name}
-            <Delete id={_user.id} />
-            <Update name={_user.name} id={_user.id} />
+        <Card key={_user.id}>
+          <p>{_user.name}</p>
+          <p>{_user.email}</p>
+          <p>{_user.phone}</p>
+          <Delete id={_user.id} />
+          <Update name={_user.name} id={_user.id} />
         </Card>)}
     </Layout>
     </>
--- frontend/src/gql/fragment-masking.ts
+++ frontend/src/gql/fragment-masking.ts
@@ -1,5 +1,6 @@
 import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
 
+
 export type FragmentType<TDocumentType extends DocumentNode<any, any>> = TDocumentType extends DocumentNode<
   infer TType,
   any
