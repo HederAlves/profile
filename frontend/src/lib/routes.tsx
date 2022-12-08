4329d789544415fe861d45edac225417df406a50
Heder Alves <alvesheder92@gmail.com>
2022-12-07 22:42:47 -0300

    Feat
    Codegen, routes and apollo

--- frontend/src/lib/routes.tsx
+++ frontend/src/lib/routes.tsx
@@ -1,14 +1,22 @@
-import {  Routes, Route } from "react-router-dom";
+import { Routes, Route, useParams } from "react-router-dom";
 import ViewNews from '../components/news/ViewNews'
 import ViewLogin from "../components/users/login/ViewLogin";
+import ViewProfile from "../components/users/profile/ViewProfile";
 import ViewRegister from "../components/users/register/ViewRegister";
-import ViewUsers from '../components/users/ViewUsers'
+import ViewUsers from "../components/users/ViewUsers";
 
-const  MainRoutes = () => {
-    return(
+interface IProps { id: string }
+
+const MainRoutes = (props: IProps) => {
+    var { id } = props;
+
+
+    return (
         <Routes>
             <Route path="/" element={<ViewNews />} />
-            <Route path="/users" element={<ViewUsers />} />
+            <Route path="users" element={<ViewUsers />}>
+                <Route path='"users"+{id}' element={<ViewProfile />} />
+            </Route>
             <Route path="/login" element={<ViewLogin />} />
             <Route path="/register" element={<ViewRegister />} />
         </ Routes>
