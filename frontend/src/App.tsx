b5831a0c8e51d875c2d763659035e80e8b5628b2
Heder Alves <alvesheder92@gmail.com>
2022-12-09 21:55:44 -0300

    Feat
    Apollo booster

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,19 @@
-import { ApolloProvider } from "@apollo/client";
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import { client } from "./lib/apollo";
-import Routes from './lib/routes'
+import { ApolloProvider } from '@apollo/client';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, RouterProvider } from 'react-router-dom';
+import { client } from './lib/apollo';
+import router from './lib/routes';
 
 function App() {
 
-  return (
-    <Router>
-      <React.StrictMode>
-        <ApolloProvider client={client}>
-          <Routes />
-        </ApolloProvider>
-      </React.StrictMode>
-    </Router>
-  )
+	return (
+		<React.StrictMode>
+			<ApolloProvider client={client}>
+				<RouterProvider router={router} />
+			</ApolloProvider>
+		</React.StrictMode>
+	);
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'));
