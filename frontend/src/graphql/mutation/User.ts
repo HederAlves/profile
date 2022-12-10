4b171510f782fb5cb20adae23008a21c1c43aaa5
Heder Alves <alvesheder92@gmail.com>
2022-12-09 21:57:45 -0300

    chore
    Apollo client

--- frontend/src/graphql/mutation/User.ts
+++ frontend/src/graphql/mutation/User.ts
@@ -1,4 +1,4 @@
-import { gql } from "@apollo/client";
+import { gql } from '@apollo/client';
 
 export const CREATE_USER = gql`
 mutation CreateUser($phone: String!, $email: String!, $password: String!, $name: String!) {
