7b3452910b7f15da85f653873a7d9d428e384fbe
Heder Alves <alvesheder92@gmail.com>
2022-12-21 23:11:47 -0300

    Feat
    migration

---
+++ backend/src/shared/infra/typeorm/migrations/1671597667647-users.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class users1671597667647 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
