9984b9b4618d5270223c929942d0538233aa660c
Heder Alves <alvesheder92@gmail.com>
2022-12-28 23:08:12 -0300

    chore

--- backend/src/service/create-users-service.ts
+++ backend/src/service/create-users-service.ts
@@ -1,14 +1,15 @@
-import { dataSource } from "../data-source"
-import { users } from "../entity/users"
+import { dataSource } from "../data-source";
+import { users } from "../entity/users";
 
-
-const myDataSource = dataSource
-const user = await myDataSource.manager.findOneBy(users, {
-    id: 1,
-})
-user.name = "Umed"
-user.password = "Umed"
-user.email = "Umed"
-user.phone = 475
-
-myDataSource.manager.save(user)
+const createUserService = dataSource
+    .createQueryBuilder()
+    .insert()
+    .into(users)
+    .values([{
+        name: 'Heder',
+        password: 'password',
+        email: 'email@example.com',
+        phone: 89798798798
+    }])
+    .execute()
+export default createUserService;
