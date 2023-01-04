1c54ac42c23e0f7d3d3c58212fb2f22a442d7d5d
Heder Alves <alvesheder92@gmail.com>
2023-01-03 23:21:46 -0300

    chore

--- frontend/src/components/atoms/NavBar.tsx
+++ frontend/src/components/atoms/NavBar.tsx
@@ -1,21 +1,19 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import imgNG from '../../../doc/images/ngi.png'
 import { ButtonNavBar, ImgNavBar, LayoutNavBar, ListButtons } from "./style";
+import { memo } from 'react'
 
 const NavBar = () => {
 
 	return (
-		<>
-			<LayoutNavBar>
-				<ImgNavBar src={imgNG} />
-				<ListButtons>
-					<li><Link to={'/usuarios'} ><ButtonNavBar>Inicio</ButtonNavBar></Link></li>
-					<li><Link to={'/'} ><ButtonNavBar>Sair</ButtonNavBar></Link></li>
-				</ListButtons>
-			</LayoutNavBar>
-			<Outlet />
-		</>
+		<LayoutNavBar>
+			<ImgNavBar src={imgNG} />
+			<ListButtons>
+				<li><Link to={'/usuarios'} ><ButtonNavBar>Inicio</ButtonNavBar></Link></li>
+				<li><Link to={'/'} ><ButtonNavBar>Sair</ButtonNavBar></Link></li>
+			</ListButtons>
+		</LayoutNavBar>
 	);
 };
 
-export default NavBar;
+export default memo(NavBar);
