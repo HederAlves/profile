65ddb8e5f454d259107858a3f68a5958ed23cb3f
Heder Alves <alvesheder92@gmail.com>
2023-01-04 22:56:27 -0300

    feat
    repository

---
+++ backend/src/repositories/usersRepository.ts
@@ -0,0 +1,14 @@
+import { EntityRepository, Repository } from 'typeorm';
+import { user } from '../model/User';
+
+
+@EntityRepository(user)
+export default class UsersRepository extends Repository<user> {
+  public async findByName(name: string): Promise<user[]> {
+    return this.find({
+      where: {
+        name,
+      },
+    });
+  }
+}
