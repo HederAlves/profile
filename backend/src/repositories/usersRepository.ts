dd20759c5db47bbb40b8b2c909fe4117da112796
Heder Alves <alvesheder92@gmail.com>
2023-01-05 22:48:37 -0300

    chore

--- backend/src/repositories/usersRepository.ts
+++ backend/src/repositories/usersRepository.ts
@@ -1,7 +1,6 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { user } from '../model/User';
 
-
 @EntityRepository(user)
 export default class UsersRepository extends Repository<user> {
   public async findByName(name: string): Promise<user[]> {
