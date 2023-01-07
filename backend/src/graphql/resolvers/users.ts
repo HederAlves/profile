d9936eca3e2e85055ab79b456bd7c56ad0bd6f1a
Heder Alves <alvesheder92@gmail.com>
2023-01-06 23:04:36 -0300

    Chore

--- backend/src/graphql/resolvers/users.ts
+++ backend/src/graphql/resolvers/users.ts
@@ -1,64 +1,113 @@
-import { Arg, Ctx, Mutation, Query, Resolver } from 'type-graphql';
-import { user } from '../../model/User';
-import { v4 as uuid } from 'uuid'
-import { Context } from 'vm';
+// import { Arg, Ctx, Mutation, Query, Resolver } from 'type-graphql';
+// import { user } from '../../model/User';
+// import { v4 as uuid } from 'uuid'
+// import { Context } from 'vm';
 
-@Resolver()
-export class UserResolver {
-    private data: user[] = [];
+import { Length, MaxLength } from "class-validator";
+import { Arg, Field, FieldResolver, InputType, Mutation, Query, Resolver, Root } from "type-graphql";
+import { Repository } from "typeorm";
+import { user } from "../../model/User";
 
-    @Query(() => [user])
-    async user() {
-        return this.data;
-    }
+// @Resolver()
+// export class UserResolver {
+//     private data: user[] = [];
 
-    @Mutation(() => user)
-    async createUser(
-        @Arg('name') name: string,
-        @Arg('password') password: string,
-        @Arg('email') email: string,
-        @Arg('phone') phone: string,
-    ) {
-        const user = { id: uuid(), name, password, email, phone };
-        this.data.push(user)
-        return user;
-    }
+//     @Query(() => [user])
+//     async user() {
+//         return this.data;
+//     }
 
-    @Mutation(_ => Boolean)
-    async deleteUser(
-        @Ctx() context: Context,
-        @Arg('id') id: string
-    ): Promise<boolean> {
-        const index = this.data.findIndex(user => user.id === id);
-        if (index > -1) {
-            this.data.splice(index, 1);
-            return true;
-        } {
-            user.deleteUser(context, id);
-            return true;
-        }
-    }
+//     @Mutation(() => user)
+//     async createUser(
+//         @Arg('name') name: string,
+//         @Arg('password') password: string,
+//         @Arg('email') email: string,
+//         @Arg('phone') phone: string,
+//     ) {
+//         const user = { id: uuid(), name, password, email, phone };
+//         this.data.push(user)
+//         return user;
+//     }
 
-    @Mutation(_ => Boolean)
-    async updateUser(
-        @Ctx() _context: Context,
-        @Arg('id') id: string,
-        @Arg('name') name: string,
-        @Arg('password') password: string,
-        @Arg('email') email: string,
-        @Arg('phone') phone: string,
-    ): Promise<boolean> {
-        const index = this.data.findIndex(user => user.id === id);
-        if (index > -1) {
-            this.data[index].name = name;
-            this.data[index].password = password;
-            this.data[index].email = email;
-            this.data[index].phone = phone;
-            return true;
-        } {
-            const user = { id, name, password, email, phone };
-            this.data.push(user);
-            return true;
-        }
-    }
+//     @Mutation(_ => Boolean)
+//     async deleteUser(
+//         @Ctx() context: Context,
+//         @Arg('id') id: string
+//     ): Promise<boolean> {
+//         const index = this.data.findIndex(user => user.id === id);
+//         if (index > -1) {
+//             this.data.splice(index, 1);
+//             return true;
+//         } {
+//             user.deleteUser(context, id);
+//             return true;
+//         }
+//     }
+
+//     @Mutation(_ => Boolean)
+//     async updateUser(
+//         @Ctx() _context: Context,
+//         @Arg('id') id: string,
+//         @Arg('name') name: string,
+//         @Arg('password') password: string,
+//         @Arg('email') email: string,
+//         @Arg('phone') phone: string,
+//     ): Promise<boolean> {
+//         const index = this.data.findIndex(user => user.id === id);
+//         if (index > -1) {
+//             this.data[index].name = name;
+//             this.data[index].password = password;
+//             this.data[index].email = email;
+//             this.data[index].phone = phone;
+//             return true;
+//         } {
+//             const user = { id, name, password, email, phone };
+//             this.data.push(user);
+//             return true;
+//         }
+//     }
+// }
+
+
+@InputType()
+export class RecipeInput {
+  @Field()
+  @MaxLength(30)
+  title: string;
+
+  @Field({ nullable: true })     
+  @Length(30, 255)
+  description?: string;
+
+  @Field(type => [String])
+  ingredients: string[];
+}
+
+
+@Resolver(of => user)
+export class RecipeResolver {
+  rateRepository: any;
+  constructor(
+    private readonly recipeRepository: Repository<user>,
+  ) {}
+
+  @Query(returns => user)
+  async recipe(@Arg("recipeId") recipeId: string) {
+    return this.recipeRepository.findOneById(recipeId);
+  }
+
+  @Mutation(returns => user)
+  async addRecipe(@Arg("recipe") recipeInput: RecipeInput) {
+    const newRecipe = this.recipeRepository.create(recipeInput);
+    return this.recipeRepository.save(newRecipe);
+  }
+
+  @FieldResolver()
+  ratings(@Root() recipe: user) {
+    return this.rateRepository.find({ recipeId: recipe.id });
+  }
+}
+
+function MaxArraySize(arg0: number) {
+    throw new Error("Function not implemented.");
 }
