64c734d89fd2462bd80564a4103b373bc91d4c78
Heder Alves <alvesheder92@gmail.com>
2023-01-07 22:48:38 -0300

    chore

--- backend/src/repositories/usersRepository.ts
+++ backend/src/repositories/usersRepository.ts
@@ -3,6 +3,18 @@ import { user } from '../model/User';
 
 @EntityRepository(user)
 export default class UsersRepository extends Repository<user> {
+  static create(user: any) {
+      throw new Error('Method not implemented.');
+  }
+  static push(user: { id: string; name: string; password: string; email: string; phone: string; }) {
+      throw new Error('Method not implemented.');
+  }
+  static splice(index: void, arg1: number) {
+      throw new Error('Method not implemented.');
+  }
+  static findIndex(arg0: (user: any) => boolean) {
+      throw new Error('Method not implemented.');
+  }
   public async findByName(name: string): Promise<user[]> {
     return this.find({
       where: {
