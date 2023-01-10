20dde76712ce373b76c8ed5a691d2eaf32a7c072
Heder Alves <alvesheder92@gmail.com>
2023-01-09 21:29:25 -0300

    chore

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -24,30 +24,3 @@ async function main(){
 }
 
 main();
-
-// import "reflect-metadata";
-// import { ApolloServer } from 'apollo-server';
-// import { buildSchema } from 'type-graphql';
-// import path = require("path");
-// import dataSource from "./data-source";
-
-// dataSource.initialize()
-
-// async function main(){
-    
-//     const schema = await buildSchema({
-//         resolvers: [
-//             `${__dirname}/graphql/resolvers/*`
-//         ],
-//         emitSchemaFile: path.resolve(__dirname, 'schema.gql'),
-//     })
-
-//     const apolloServer = new ApolloServer({
-//         schema
-//     })
-
-//     await apolloServer.listen(4000)
-//     console.log(`Server running on port 4000`);
-// }
-
-// main();
